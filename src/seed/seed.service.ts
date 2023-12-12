ccd7d427065376257babd110fdc98f7282d85aef
Camilo Mazorra <mazorra006@gmail.com>
2023-12-12 15:28:12 -0500

    Insertar seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from './../products/products.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
@@ -13,7 +14,14 @@ export class SeedService {
 
   private async deployDb() {
     await this.productService.deleteAllProducts();
-
+    const products = initialData.products;
+    const insertPromises = [];
+    products.forEach( product => {
+      // Esto funciona porque el producto luce como el craeteProductDto
+      insertPromises.push( this.productService.create(product) )
+    });
+    // Espera a que todas las promesas se resuelvan
+    await Promise.all( insertPromises );
     return true;
   }
   
