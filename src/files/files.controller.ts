00c7b1ea57eabc1490684b31e672c5e321e32e41
Camilo Mazorra <mazorra006@gmail.com>
2023-12-26 00:02:09 -0500

    devolviendo el path del archivo

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Post, UploadedFile, UseInterceptors, BadRequestException, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors, BadRequestException, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator, Get, Param, Res } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter, fileNamer } from './helpers';
 import { diskStorage } from 'multer';
+import { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
+  @Get('product/:imageName')
+  findOneProduct(
+    // Uso este decorador para tomar el control de la respuesta que quiero obtener en el endpoint
+    // Hay que tener cuidado porque se puede saltar interceptores y otras cosas
+    @Res() res: Response,
+    @Param('imageName') imageName: string
+  ) {
+    const path = this.filesService.getStaticProductImg( imageName );
+    // Envio el archivo que esta en este path, si la imagen no existe no la envia
+    res.sendFile( path )
+  }
 
   // ! Para los archivos lo mejor es guardarlos en otra parte, aca lo vamos a hacer en el filesystem
   // ! lo cual es una practica que se DEBE evitar a toda costa, por motivos de seguridad
@@ -35,11 +47,14 @@ export class FilesController {
     if ( !file ) {
       throw new BadRequestException('Make sure the file is an image');
     }
+    const secureUrl = `${ file.filename }`
     // Lo que se debe hacer es no guardar en el fileSystem, sino en otro lugar
     // No quiero que las imagenes esten en el mismo lugar donde esta el codigo de mi aplicacion
     return {
-      fileName: file.originalname
+      secureUrl
     };
   }
 
+
+  
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,7 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { BadGatewayException, Injectable } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 
 @Injectable()
 export class FilesService {
 
+    getStaticProductImg( imageName: string ) {
+
+        const path = join( __dirname, '../../static/products', imageName );
+        // se verifica que exista el archivo, no devolver mucha informacion por seguridad
+        if ( !existsSync(path) ) 
+            throw new BadGatewayException(`No product found with image ${ imageName }`);
+        return path;
+    }
+    
 }
